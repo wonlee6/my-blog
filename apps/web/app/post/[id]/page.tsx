1bc7206185aa9b88e70ac9c9fff702ec055db3e5
wonlee6 <wonlee6@gmail.com>
2025-01-01 15:05:23 +0900

    add: post metadata setup

--- apps/web/app/post/[id]/page.tsx
+++ apps/web/app/post/[id]/page.tsx
@@ -1,36 +1,52 @@
+import type { Metadata, ResolvingMetadata } from 'next'
+
 import { notFound } from 'next/navigation'
 
 import { allPosts } from '@/.contentlayer/generated'
 
-// import type { Metadata, ResolvingMetadata } from 'next'
+type Props = {
+  params: Promise<{ id: string }>
+}
 
-// type Props = {
-//   params: Promise<{ id: string }>
-//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-// }
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = (await params).id
+  const post = allPosts.find((p) => p._raw.flattenedPath === id)
 
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   // read route params
-//   const id = (await params).id
+  if (!post) {
+    return {
+      title: 'Post Not Found'
+    }
+  }
 
-//   // fetch data
-//   // const product = await fetch(`https://.../${id}`).then((res) => res.json())
+  const previousImages = (await parent).openGraph?.images || []
 
-//   // optionally access and extend (rather than replace) parent metadata
-//   // const previousImages = (await parent).openGraph?.images || []
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: 'sang won', url: `https://wonlee1205-blog.vercel.app/post/${id}` }],
+    creator: 'sang won',
+    keywords: post.tags,
+    openGraph: {
+      type: 'article',
+      countryName: 'South Korea',
+      locale: 'ko',
+      description: post.description,
+      title: post.title,
+      url: `https://wonlee1205-blog.vercel.app/post/${id}`,
+      images: [...previousImages]
+    },
+    metadataBase: new URL(`https://wonlee1205-blog.vercel.app/post/${id}`)
+  }
+}
 
-//   return {
-//     title: product.title,
-//     openGraph: {
-//       images: ['/some-specific-page-image.jpg', ...previousImages],
-//     },
-//   }
-// }
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
+}
 
-export default async function PostPage({ params }: { params: Promise<{ id: string }> }) {
+export default async function PostPage({ params }: Props) {
   const slug = (await params).id
   const findPost = allPosts.find((i) => i._raw.flattenedPath === slug)
 
